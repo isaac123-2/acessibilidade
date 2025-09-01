34518f7db64c5f76ebd93edb18b419a2e36c8c7f
isaac123-2 <00001123082091sp@Al.educacao.sp.gov.br>
2025-09-01 17:34:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const botaoDeAcessibilidade = document.getElementById('Botao-Acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('Opcoes-Acessibilidades')
+
+    botaoDeAcessibilidade.addEventListener('click',function(){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao')
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+    })
+
+    const aumentaFonteBotao = document.getElementById('Aumentar-Fonte');
+    const diminuiFonteBotao = document.getElementById('Diminuir-Fonte');
+
+    let tamanhoAtualFonte = 1;
+
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
+
+    diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
+})
