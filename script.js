6463aed5000f365947079ebde3e519fa02d32583
isaac123-2 <00001123082091sp@Al.educacao.sp.gov.br>
2025-09-15 19:05:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ document.addEventListener('DOMContentLoaded', function(){
     botaoDeAcessibilidade.addEventListener('click',function(){
         botaoDeAcessibilidade.classList.toggle('rotacao-botao')
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+
+        const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') ==='true';
+        botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
     })
 
     const aumentaFonteBotao = document.getElementById('Aumentar-Fonte');
     const diminuiFonteBotao = document.getElementById('Diminuir-Fonte');
+
+    const alternaconstraste = document.getElementById('alterna-contraste');
 
     let tamanhoAtualFonte = 1;
 
@@ -21,4 +26,13 @@ document.addEventListener('DOMContentLoaded', function(){
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
+
+    alternaconstraste.addEventListener('click', function(){
+        document.body.classList.toggle('alternaconstraste')
+    })
 })
+
+ScrollReveal().reveal('#inicio',{ delay:500});
+ScrollReveal().reveal('#tropicalia',{ delay:500});
+ScrollReveal().reveal('#galeria',{ delay:500});
+ScrollReveal().reveal('#contato',{ delay:500});
